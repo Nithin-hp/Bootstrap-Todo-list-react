b4b9af4637105cc88cea8a11913e3a2914de7067
Nithin H P <71778546+Nithin-hp@users.noreply.github.com>
2020-11-10 22:58:37 +0530

    Update TodoList.js

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,26 +3,32 @@ import TodoItem from "./TodoItem";
 export default class TodoList extends Component {
   render() {
     const { items, clearList, handleDelete, handleEdit } = this.props;
+    let display=" "
+    if(items.length)
+    {
+      display=(
+        <div> {items.map(item => {
+        return (
+          <TodoItem
+            key={item.id}
+            title={item.title}
+            handleDelete={() => handleDelete(item.id)}
+            handleEdit={() => handleEdit(item.id)}
+          />
+        );
+      })}
+      <button type="button"
+      className="btn btn-danger btn-block text-uppercase mt-5"
+      onClick={clearList}> clear list</button>
+     </div>
+      )
+    }
     return (
       <ul className="list-group my-5">
-        <h3 className="text-capitalize text-center">todo list</h3>
-        {items.map(item => {
-          return (
-            <TodoItem
-              key={item.id}
-              title={item.title}
-              handleDelete={() => handleDelete(item.id)}
-              handleEdit={() => handleEdit(item.id)}
-            />
-          );
-        })}
-        <button
-          type="button"
-          className="btn btn-danger btn-block text-uppercase mt-5"
-          onClick={clearList}
-        >
-          clear list
-        </button>
+        {items.length ?
+        <h3 className="text-capitalize text-center">todo list</h3>: null}
+        {display}
+       
       </ul>
     );
   }
